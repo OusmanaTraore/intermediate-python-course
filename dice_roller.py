d8d69ce2eb1563e0326f2eb737e2a2c4edadd762
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-02-16 11:03:16 +0100

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,10 @@
 import random
-roll = random.randint(1,6)
+#roll = random.randint(1,6)
 
 def main():
+  roll = 5
   print(f'You rolled a {roll}')
+  #print(f'You rolled a {roll}')
   
 
 if __name__== "__main__":
